7507fc66020000551effae65df5a4709ac17a69a
Stylix <anhduongfa@gmail.com>
2025-11-03 07:03:07 +0700

    feat: adding garbage collector

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -20,6 +20,13 @@
   networking.hostName = "laptop"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  # Garbage collector
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+    };
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
@@ -76,7 +83,7 @@
     # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
-
+    
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
