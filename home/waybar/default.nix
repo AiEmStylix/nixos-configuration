088a55075c82e55e346db5cfbf6e677078a41f23
Stylix <anhduongfa@gmail.com>
2025-11-04 13:44:51 +0700

    feat: adding some modules to the right of waybar

--- home/waybar/default.nix
+++ home/waybar/default.nix
@@ -11,6 +11,14 @@
           "clock"
         ];
 
+        modules-right = [
+          "tray"
+          "bluetooth"
+          "network"
+          "pulseaudio"
+          "battery"
+        ];
+
         "clock" = {
           format = "{:%A %d/%m/%Y - %H:%M}";
           tooltip-format = "<span font_family='monospace'>{calendar}</span>"; # Use span with monospace for teletype text
