7b876d7e96ec7eda96049dae89f7a4d68730acaf
Stylix <anhduongfa@gmail.com>
2025-11-08 07:34:23 +0700

    chore: auto start noctalia-shell and little config in fcitx5

--- users/stylix/home.nix
+++ users/stylix/home.nix
@@ -54,25 +54,7 @@
     
   programs.noctalia-shell = {
     enable = true;
-    colors = {
-        # you must set ALL of these
-      mError = "#dddddd";
-      mOnError = "#111111";
-      mOnPrimary = "#111111";
-      mOnSecondary = "#111111";
-      mOnSurface = "#828282";
-      mOnSurfaceVariant = "#5d5d5d";
-      mOnTertiary = "#111111";
-      mOutline = "#3c3c3c";
-      mPrimary = "#aaaaaa";
-      mSecondary = "#a7a7a7";
-      mShadow = "#000000";
-      mSurface = "#111111";
-      mSurfaceVariant = "#191919";
-      mTertiary = "#cccccc";
     };
-      # this may also be a string or a path to a JSON file.
-  };
   
   home.stateVersion = "25.05";
 }
